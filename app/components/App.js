3a9566f66d89069a08d498000a46e376f293b5f8
Josh <josh4locations@gmail.com>
2017-12-30 12:42:04 -0800

    add wrapper component

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
 // Whole application
-export default () => (
+export default props => (
     <div>
+        <h1>SearchBar</h1>
+        <h1>
+            { props.hasSearched ? <div>beenSearched</div> : <div>nothing</div> }
+        </h1>
         { /* search bar and nothing/results    */ }
     </div>
 );
--- app/components/App.test.js
+++ app/components/App.test.js
@@ -1,35 +1,42 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import expect from 'expect';
 import App from './App';
+// import SearchBar from './SearchBar';
+// import Content from './Content';
 
 configure({ adapter: new Adapter() });
 
 describe('App Component', () => {
-    // lets us keep track of current props/context as well as cleaning it after each test
-    let props;
-    let mountedApp;
-    const app = () => {
-        if (!mountedApp) {
-            mountedApp = mount(
-                <App {...props} />
-            );
-        }
-        return mountedApp;
+  // lets us reset App after each test
+  let shallowApp;
+  const app = () => {
+    if (!shallowApp) {
+      shallowApp = shallow(<App />);
     }
+    return shallowApp;
+  };
 
-    beforeEach(() => {
-        props = {};
-        mountedApp = undefined;
+  beforeEach(() => {
+    shallowApp = undefined;
+  });
+
+  // app should always render a div
+  it('always returns a div', () => {
+    const divs = app().find('div');
+    expect(divs.length).toBeGreaterThan(0);
+  });
+
+  describe('The rendered div', () => {
+    it('should have two children', () => {
+      expect(app().children().length).toEqual(2);
     });
-
-    // app should always render a div
-    it('always returns one div', () => {
-        const divs = app().find('div');
-        
-        expect(divs.length).toEqual(1);
+    xit('should render a search bar', () => {
+    //   expect(app().children().find(SearchBar).length).toEqual(1);
     });
-
-    // expect that div to render other components... 
+    xit('should render main area', () => {
+    //   expect(app().children().find(Content).length).toEqual(1);
+    });
+  });
 });
