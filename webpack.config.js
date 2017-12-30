87df38c5a23faba56e16bc7b2116b67505f90498
Josh <josh4locations@gmail.com>
2017-12-29 18:40:50 -0800

    ignore build folder and add config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+  // This is the entry point or start of our react application
+  entry: './app/app.js',
+
+  // The plain compiled JavaScript will be output into this file
+  output: {
+    filename: 'build/bundle.js',
+  },
+
+  // This section describes the transformations we will perform
+  module: {
+    loaders: [
+      {
+        // Only working with files that are in a .js or .jsx extension
+        test: /\.jsx?$/,
+        // Webpack will only process files in our app folder. This avoids processing
+        // unnecessary files
+        include: /app/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          // These are the specific transformations we'll be using.
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
+  // Without this the console says all errors are coming from just coming from bundle.js
+  devtool: 'eval-source-map',
+};
