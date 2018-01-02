8bbd6d5233906d08c50925d1fe43a67bc3228133
Josh <josh4locations@gmail.com>
2018-01-01 23:04:44 -0800

    linting

--- app/api/api.js
+++ app/api/api.js
@@ -5,7 +5,7 @@ export default (search) => {
   const trendOrSearch = !search ?
     fetch('http://api.giphy.com/v1/gifs/trending?api_key=iy5kPHVlbDexRE46UhbuiwvOr3fej1Af') :
     fetch(`http://api.giphy.com/v1/gifs/search?api_key=iy5kPHVlbDexRE46UhbuiwvOr3fej1Af&q=${search.replace(' ', '+')}`);
-  
+
     // get data from giphy api
   // eslint-disable-next-line
   return trendOrSearch
