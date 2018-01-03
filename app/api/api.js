52b64f152251dcca4b1642cc0de6465220722ca7
Josh <josh4locations@gmail.com>
2018-01-02 20:47:13 -0800

    fix error in reducer and restructured api condition

--- app/api/api.js
+++ app/api/api.js
@@ -3,12 +3,12 @@ import fetch from 'isomorphic-fetch';
 export default (search) => {
   // ternary for trending or search
   const trendOrSearch = !search ?
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=iy5kPHVlbDexRE46UhbuiwvOr3fej1Af') :
-    fetch(`http://api.giphy.com/v1/gifs/search?api_key=iy5kPHVlbDexRE46UhbuiwvOr3fej1Af&q=${search.replace(' ', '+')}`);
+    'https://api.giphy.com/v1/gifs/trending?api_key=iy5kPHVlbDexRE46UhbuiwvOr3fej1Af' :
+    `https://api.giphy.com/v1/gifs/search?api_key=iy5kPHVlbDexRE46UhbuiwvOr3fej1Af&q=${search.replace(' ', '+')}`;
 
     // get data from giphy api
   // eslint-disable-next-line
-  return trendOrSearch
+  return fetch(trendOrSearch)
     .then(data => data.json())
     .then(data => data)
     .catch(err => err);
